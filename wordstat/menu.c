4a7e8778b18fcc11e31d2c81ad9c3fe8ad8ee775
Penny Rowe <prowe@harbornet.com>
2023-01-21 19:28:57 +0000

    Initialize menu option numbers

--- wordstat/menu.c
+++ wordstat/menu.c
@@ -8,8 +8,14 @@
 #include <stdio.h>
 #include "menu.h"
 
-int getMenuOption() {
+int getMenuOption()
+{
 	int opt;
+	int MENU_STATS = 1;
+	int MENU_HISTO = 2;
+	int MENU_INPUT = 3;
+	int MENU_EXIT = 4;
+
 	printf("*** WORD STATS MENU ***\n");
 	printf("Enter %d to print vowel and consonant frequency.\n", MENU_STATS);
 	printf("Enter %d to print histogram.\n", MENU_HISTO);
