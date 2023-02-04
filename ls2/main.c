491ad52eb566e55568555ae16cd998f202fe501b
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-04 00:58:09 +0000

    Add todo list

--- ls2/main.c
+++ ls2/main.c
@@ -7,10 +7,21 @@
  * Main function
  * Usage: ls2 <path> [exact-match-pattern]
  */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	// stack stores the lines to print out
 	stack_t *s = initstack();
 
+	// TODO: Parse the inputs
+
+	// TODO: Get the contents of path and put it in the stack
+
+	// TODO: Loop over the path contents and see what the file type is ('.', dir, file, link, etc)
+	// TODO: For every directory, repeat lines 17 - 20
+
+	// TODO: Case 1: Only path entered
+
+	// TODO: Case 2: pattern also entered
 	push(s, "Hello1");
 	push(s, "Hello2");
 	push(s, "Hello3");
