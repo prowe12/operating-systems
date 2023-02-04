0d470c15bcf1c50dc85171df0fdde2b06e145696
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-04 00:57:56 +0000

    Fix error where stack is freed twice

--- ls2/stack.c
+++ ls2/stack.c
@@ -6,8 +6,9 @@
  * Creates and returns a pointer to a new stack
  * @return pointer to a stack_t
  */
-stack_t *initstack() {
-  stack_t *newStack = (stack_t*) malloc(sizeof(stack_t));
+stack_t *initstack()
+{
+  stack_t *newStack = (stack_t *)malloc(sizeof(stack_t));
   newStack->top = NULL;
   newStack->size = 0;
   return newStack;
@@ -18,13 +19,15 @@ stack_t *initstack() {
  * @param s pointer to a stack_t
  * @return a pointer to the top, or NULL if stack was empty
  */
-void* pop(stack_t* s) {
-  if (s->size > 0) {
+void *pop(stack_t *s)
+{
+  if (s->size > 0)
+  {
     stacknode_t *top = s->top;
-    void* oldData = top->data;  // save the current top for return
+    void *oldData = top->data; // save the current top for return
     s->top = s->top->next;
     s->size--;
-    free(top);  // free up memory
+    free(top); // free up memory
     return oldData;
   }
   return NULL;
@@ -35,8 +38,9 @@ void* pop(stack_t* s) {
  * @param s pointer to a stack_t
  * @param item pointer to an element to push onto the stack
  */
-void push(stack_t* s, void* item) {
-  stacknode_t *newNode = (stacknode_t*) malloc(sizeof(stacknode_t));
+void push(stack_t *s, void *item)
+{
+  stacknode_t *newNode = (stacknode_t *)malloc(sizeof(stacknode_t));
   newNode->data = item;
   newNode->next = s->top;
   s->top = newNode;
@@ -47,10 +51,11 @@ void push(stack_t* s, void* item) {
  * Frees up the stack
  * @param s pointer to a stack_t
  */
-void freestack(stack_t* s) {
-  while (s->size) {
-    void* ret  = pop(s);
-    free(ret);
+void freestack(stack_t *s)
+{
+  while (s->size)
+  {
+    pop(s);
   }
   free(s);
 }
@@ -59,10 +64,12 @@ void freestack(stack_t* s) {
  * Prints out the contents of the stack from top to bottom.
  * @param s pointer to a stack_t
  */
-void printstack(stack_t* s) {
+void printstack(stack_t *s)
+{
   stacknode_t *current = s->top;
-  while (current != NULL) {
-    printf("%s\n", (char*) current->data);
+  while (current != NULL)
+  {
+    printf("%s\n", (char *)current->data);
     current = current->next;
   }
 }
