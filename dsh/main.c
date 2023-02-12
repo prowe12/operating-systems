c4c068cb4567060dc770daa6d3925e0831c47829
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-02-12 23:49:07 +0000

    Write code to split the input string, with test function

--- dsh/main.c
+++ dsh/main.c
@@ -12,18 +12,106 @@
 #include <string.h>
 #include "dsh.h"
 
+/**
+ * Split a string into tokens
+ * @params str  The string to split
+ * @params delim  The delimiter to split on
+ * @returns  An array of tokens
+ */
+char **split(char *str, char *delim)
+{
+	int CAP = 256;			// Max string length
+	char str3[strlen(str)]; // Cannot use strtok with char*
+
+	// Count the number of delimters in str
+	int numtokens = 0;
+	int i = 0;
+	while (str[i])
+	{
+		str3[i] = str[i];
+
+		if (str[i] == delim[0])
+		{
+			numtokens++; // Increment for each delimiter
+						 // printf("delim: ");
+		}
+		// printf("%c\n", str[i]);
+		i++;
+	}
+	numtokens++; // There is one more token than there are delimters
+	// printf("There are %d tokens\n", numtokens);
+
+	// Create a 2d array of characters: that is, number of pointers to strings
+	char **array = (char **)malloc((numtokens + 1) * sizeof(char *));
+
+	// Setup the token
+	char *token;
+	token = strtok(str3, delim);
+	// printf("%s\n", token);
+
+	// Loop through each array element and instantiate an array
+	// of capacity cap
+	for (int i = 0; i < numtokens; i++)
+	{
+		array[i] = (char *)malloc(CAP * sizeof(char));
+		strcpy(array[i], token);
+		token = strtok(NULL, delim); // What is this???
+	}
+	array[numtokens] = (char *)malloc(CAP * sizeof(char));
+	array[numtokens] = NULL;
+
+	return array;
+}
+
+/**
+ * Print the array
+ * @params array  The array
+ */
+void printarray(char **array)
+{
+
+	int i = 0;
+	while (array[i])
+	{
+		printf("%s\n", array[i]);
+		i++;
+	}
+	return;
+}
+
+/**
+ * Test the split functions
+ */
+void testSplit()
+{
+	// char *line = (char *)malloc(256); // empty buffer to store the input
+	char *line = "hello there Penny";
+
+	// Tokenize user input
+	char **array = split(line, " ");
+
+	// Print the results
+	printf("\nBack in testSplit, printing the array again\n");
+	printarray(array);
+}
+
 int main(int argc, char **argv)
 {
-	char cmdline[MAXBUF]; // stores user input from commmand line
+
+	testSplit();
+
+	// TODO: undo following comment
+	// char cmdline[MAXBUF]; // stores user input from commmand line
 
 	// Print the prompt
 	printf("dsh> ");
 
-	// Accept user input
-	char *line = (char *)malloc(256); // empty buffer to store the input
-	fgets(line, 256, stdin);		  // read up to 256 chars into buffer
+	// TODO: Uncomment this line
+	// fgets(cmdline, 256, stdin);		  // read up to 256 chars into buffer
 
-	// Print the results
-	printf("%s", line);
-	return 0;
+	// TODO: Delete this line
+	printf("\n");
+
+	// TODO: Uncomment fgets line; delete line =
+	// Accept user input
 }
