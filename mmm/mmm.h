a1063b565740eb9b157b0d72c757d591f2cd8c4e
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-06 18:33:02 +0000

    Add struct for threads

--- mmm/mmm.h
+++ mmm/mmm.h
@@ -7,6 +7,14 @@
 extern int **mat1, **mat2, **mat3, **mat4;
 extern int matdim;
 
+/** struct to hold parameters to mmm_par() **/
+typedef struct thread_args
+{
+    int tid;    // the given thread id (0, 1, ...)
+    long first; // first row
+    long last;  // last row
+} thread_args;
+
 void mmm_init();
 void mmm_reset(int **);
 void mmm_freeup();
